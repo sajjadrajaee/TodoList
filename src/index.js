770dfe350d494d98e0d6e09d23f8ecfc1129ce0e
Sajad Rajaee <sajjadrajaee@gmail.com>
2022-07-07 02:14:03 +0430

    Add todo basic

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
 import _ from 'lodash';
 import './style.css';
 
-function component() {
-	const element = document.createElement('div');
+let todos = [
+	{
+		index:1,
+		description: "I must complete the project",
+		status: false
+	},
+	{
+		index:2,
+		description: "I must read lessons",
+		status: false
+	},
+	{
+		index:3,
+		description: "I must read a book",
+		status: true
+	}
+];
 
-	// Lodash, now imported by this script
-	
-	return element;
+class Todo {
+	constructor(index, description, status){
+		this.description = description;
+		this.status = status;
+		this.index = index;
+	}
 }
 
-document.body.appendChild(component());
+const todoElement = document.querySelector('.card');
+todos.forEach((todo) => {
+	
+	if(todo.status===false){
+		const prop = document.createElement('div');
+		prop.classList.add('todo');
+		prop.innerHTML = `
+		<div class="todo-text"><input type="checkbox">${todo.description}</div>
+		<div class="icon"><img src="./../assets/refresh.png"></div>
+		`
+		todoElement.appendChild(prop);
+	}
+
+});
