547f0a6fc0f2dc7da95423e65a4a97f1b46c8d95
Sajad Rajaee <sajjadrajaee@gmail.com>
2022-07-07 13:54:00 +0430

    Add linters

--- src/index.js
+++ src/index.js
@@ -1,43 +1,42 @@
-import _ from 'lodash';
 import './style.css';
 
-let todos = [
-	{
-		index:1,
-		description: "I must complete the project",
-		status: false
-	},
-	{
-		index:2,
-		description: "I must read lessons",
-		status: false
-	},
-	{
-		index:3,
-		description: "I must read a book",
-		status: true
-	}
+const todos = [
+  {
+    index: 1,
+    description: 'I must complete the project',
+    status: false,
+  },
+  {
+    index: 2,
+    description: 'I must read lessons',
+    status: false,
+  },
+  {
+    index: 3,
+    description: 'I must read a book',
+    status: true,
+  },
 ];
 
 class Todo {
-	constructor(index, description, status){
-		this.description = description;
-		this.status = status;
-		this.index = index;
-	}
+  constructor(index, description, status) {
+    this.description = description;
+    this.status = status;
+    this.index = index;
+  }
 }
 
 const todoElement = document.querySelector('.card');
 todos.forEach((todo) => {
-	
-	if(todo.status===false){
-		const prop = document.createElement('div');
-		prop.classList.add('todo');
-		prop.innerHTML = `
-		<div class="todo-text"><input type="checkbox">${todo.description}</div>
-		<div class="icon"><img src="./../assets/refresh.png"></div>
-		`
-		todoElement.appendChild(prop);
-	}
-
+  if (todo.status === false) {
+    const prop = document.createElement('div');
+    prop.classList.add('todo');
+    prop.innerHTML = `
+<div class="todo-text"><input type="checkbox">${todo.description}</div>
+<div class="icon"><img src="./../assets/refresh.png"></div>
+`;
+    todoElement.appendChild(prop);
+  }
 });
+
+export default Todo;
